68dd54ca6b9db009a782a28921532bb656e7091f
Stef Spermalie <stef.vercaemer@spermalie.be>
2020-05-05 16:05:19 +0200

    Declare variables and links for the DOM-elements.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,46 @@
 const RdbSemesterNaam = 'rdbSemester';
 const ChkLectorenNaam = 'chkLectoren';
 
-window.addEventListener('load', Initieer);
+var btnAdd, btnDelete, btnSave;
+var divButtons, divContainer, divEdit, divModus, divNav, divShow;
 
-function Initieer() {
+window.addEventListener('load', Initieer);
+
+function Initieer() {
+	KoppelElementen();
+	KoppelEvents();
 
 };
+
+const KoppelElementen = () => {
+btnAdd = document.getElementById("btnAdd");
+
+btnDelete = document.getElementById("btnDelete");
+
+btnSave = document.getElementById("btnSave");
+
+divButtons = document.getElementById("divButtons");
+
+divContainer = document.getElementById("divContainer");
+
+divEdit = document.getElementById("divEdit");
+
+divModus = document.getElementById("divModus");
+
+divNav = document.getElementById("divNav");
+
+divShow = document.getElementById("divShow");
+
+}
+
+const KoppelEvents = () => {
+	btnAdd.addEventListener('click', () => {
+                        
+	});
+	btnDelete.addEventListener('click', () => {
+                        
+	});
+	btnSave.addEventListener('click', () => {
+                        
+	});
+}
